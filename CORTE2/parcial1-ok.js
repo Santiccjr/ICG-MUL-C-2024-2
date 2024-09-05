37fa20f53135ab6c7d220537409c8db5e8ee4fe1
Santiccjr <153877444+Santiccjr@users.noreply.github.com>
2024-09-04 23:16:48 -0500

    Modificacion clases
    Se agrego una clase para manejar las coordenadas cartesianas, además de hacer que el polígono siempre salga con su base de manera recta

--- CORTE2/parcial1-ok.js
+++ CORTE2/parcial1-ok.js
@@ -1,58 +1,61 @@
+class Cartesiano {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    sumar(punto) {
+        return new Cartesiano(this.x + punto.x, this.y + punto.y);
+    }
+}
+
 class Poligonos {
-    #x;
-    #y;
+    #centro;
     #n;
     #l;
 
     constructor(x, y, n, l) {
-        this.#x = x;
-        this.#y = y;
+        this.#centro = new Cartesiano(x, y); 
         this.#n = n;
         this.#l = l;
     }
 
-    cart2polar(x, y) {
-        const r = Math.sqrt(x * x + y * y);
-        const angle = Math.atan2(y, x);
-        return [r, angle];
+    polar2cart(r, angle) {
+        const x = r * Math.cos(angle);
+        const y = r * Math.sin(angle);
+        return new Cartesiano(x, y);
     }
-
-    polar2cart(r, angle, i, d) {
-        const x = r * Math.cos(i * d + angle);
-        const y = r * Math.sin(i * d + angle);
-        return [x, y];
-    }
-
 
     findAngle(n) {
         return 2 * Math.PI / n;
     }
-
 
     Poligono() {
         const d = this.findAngle(this.#n); 
         const r = this.#l / (2 * Math.sin(Math.PI / this.#n)); 
         const vertices = [];
 
+        // Ajustamos el ángulo inicial para que la base esté horizontal en la parte inferior
+        const initialAngle = -Math.PI / 2;
+
         for (let i = 0; i < this.#n; i++) {
-            const angle = i * d; 
-            const [vx, vy] = this.polar2cart(r, angle, 0, 0); 
-            vertices.push([this.#x + vx, this.#y + vy]); 
+            const angle = i * d + initialAngle; // Ajustamos el ángulo inicial
+            const vertice = this.polar2cart(r, angle); 
+            vertices.push(this.#centro.sumar(vertice)); 
         }
 
         this.Dibujar(vertices);
     }
-
 
     Dibujar(vertices) {
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
-        ctx.moveTo(vertices[0][0], vertices[0][1]);
+        ctx.moveTo(vertices[0].x, vertices[0].y);
 
         for (let i = 1; i < vertices.length; i++) {
-            ctx.lineTo(vertices[i][0], vertices[i][1]);
+            ctx.lineTo(vertices[i].x, vertices[i].y);
         }
 
         ctx.closePath();
